5efd72a8fa4b725675e8e20705a6ab513eaaa61e
Brase2080 <tom.sonnerat@oteria.fr>
2024-12-09 12:09:11 +0100

    Update database.sql
    - Updated the whole database

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,44 +1,56 @@
-CREATE TABLE IF NOT EXISTS members (
-idmember INTEGER PRIMARY KEY AUTOINCREMENT,
-familyname VARCHAR(30) NOT NULL,
-name VARCHAR(20) NOT NULL,
-class VARCHAR(2) NOT NULL,
-role VARCHAR(15),
-dateinscription DATE NOT NULL,
-givenplant VARCHAR(30),
-FOREIGN KEY(planteattribuee) REFERENCES owner
-FOREIGN KEY(class) REFERENCES classname)
-
-CREATE TABLE IF NOT EXISTS plant (
-idplant INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(50),
-especes VARCHAR(50),
-status VARCHAR(20),
-location VARCHAR(50),
-humidity FLOAT,
-temperature FLOAT,
-luminosity FLOAT,
-owner VARCHAR(50),
-FOREIGN KEY(owner) REFERENCES members(idmember)
+CREATE TABLE IF NOT EXISTS classe (
+    Nom_classe VARCHAR(3) PRIMARY KEY,
+    Agenda VARCHAR(255),
+    Liste_membre VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS rapport ( 
-moisrapport PRIMARY KEY,
-humidity FLOAT,
-temperature FLOAT)
-
-CREATE TABLE IF NOT EXISTS classes (
-nomclasse VARCHAR(3) PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS membre (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Cle_API VARCHAR NOT NULL UNIQUE,
+    Nom VARCHAR(50) NOT NULL,
+    Prenom VARCHAR(30) NOT NULL,
+    Classe VARCHAR(3) NOT NULL,
+    Role_Association VARCHAR(30) NOT NULL DEFAULT 'Membre',
+    Photo_profil VARCHAR(50) NOT NULL DEFAULT 'default',
+    Date_inscription VARCHAR(10) NOT NULL,
+    Plante_Principale INT,
 )
 
-CREATE TABLE IF NOT EXISTS classe (
-idclass VARCHAR(3) PRIMARY KEY AUTOINCREMENT,
-membername VARCHAR(15),
-numberstudent INTEGER)
+CREATE TABLE IF NOT EXISTS plante (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nom VARCHAR(50) NOT NULL,
+    Type_Plante VARCHAR(50) NOT NULL,
+    Statut VARCHAR(20) DEFAULT 'Normale',
+    Localisation VARCHAR(25) NOT NULL,
+    Humidite FLOAT DEFAULT 0.0,
+    Temperature FLOAT DEFAULT 0.0,
+    Luminosite FLOAT DEFAULT 0.0,
+    Derniere_Photo VARCHAR(50) DEFAULT 'None',
+    Superviseur INT NOT NULL DEFAULT 0,
+    CONSTRAINT fk_superviseur FOREIGN KEY (Superviseur) REFERENCES membre(Id)
+)
 
-CREATE TABLE IF NOT EXISTS INTERVENTION (
-idintervention INTEGER PRIMARY KEY AUTOINCREMENT,
-plant VARCHAR(30),
-intervant VARCHAR(70),
-mission VARCHAR(50),
-date DATE)
+CREATE TABLE IF NOT EXISTS intervention (
+    Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Date_intervention VARCHAR(20),
+    Id_intervenant INT NOT NULL DEFAULT 0,
+    Id_Plante INT NOT NULL DEFAULT 0,
+    Note VARCHAR(250) DEFAULT '',
+    CONSTRAINT fk_intervenant FOREIGN KEY (Id_intervenant) REFERENCES membre(Id),
+    CONSTRAINT fk_plante FOREIGN KEY (Id_Plante) REFERENCES plante(Id)
+)
+
+CREATE TABLE IF NOT EXISTS rapport (
+    Date_Rapport VARCHAR(7) PRIMARY KEY?
+    Id_Plante INT NOT NULL DEFAULT 0,
+    Histo_Hum VARCHAR(255) DEFAULT '',
+    Histo_Temp VARCHAR(255) DEFAULT '',
+    Histo_Lum VARCHAR(255) DEFAULT '',
+    Histo_Photo VARCHAR(50),
+    CONSTRAINT fk_id_plante FOREIGN KEY (Id_Plante) REFERENCES plante(Id)
+)
+
+CREATE TABLE IF NOT EXISTS Cartes (
+    Identifier VARCHAR(50) NOT NULL PRIMARY KEY,
+    Plantes VARCHAR(20) DEFAULT ''
+)
